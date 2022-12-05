c7aa454bb32442f4108bf80edeb778fddd50fbcd
JRToyoda <therubikators@gmail.com>
2022-12-05 18:58:02 +0800

    Version 2

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 i = 0
 total = 0
-num = input("Enter a number or press enter to quit: ")
+num = input("Enter a number or press Enter to quit: ")
 
 while num:
     i += 1
     total += float(num)
-    num = input("Enter a number or press enter to quit: ")
+    num = input("Enter a number or press Enter to quit: ")
 
 print("The sum is", total, "\nThe average is", total / i)
