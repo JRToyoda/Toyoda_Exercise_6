2a5941f40ca9a45652fdd05be4f1108ab3b8c9cc
JRToyoda <therubikators@gmail.com>
2022-12-05 18:41:56 +0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+i = 0
+total = 0
+num = input("Enter a number or press enter to quit: ")
+
+while num:
+    i += 1
+    total += float(num)
+    num = input("Enter a number or press enter to quit: ")
+
+print("The sum is", total, "\nThe average is", total / i)
