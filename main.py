9ac87fc4db782b7d0879395000a6cb06cd5889d6
JRToyoda <therubikators@gmail.com>
2022-12-14 15:51:38 +0800

    Version 2

--- main.py
+++ main.py
@@ -1,21 +1,15 @@
-def new():
+i = 0
+total = 0
+num = input("Enter a number or press Enter to quit: ")
 
-    i = 0
-    total = 0
-    num = input("Enter a number or press Enter to quit: ")
+if num == "":
+    quit()
 
-    if num == "":
-        print("Please input a number")
-        new()
+else:
 
-    else:
+    while num:
+        i += 1
+        total += float(num)
+        num = input("Enter a number or press Enter to quit: ")
 
-        while num:
-            i += 1
-            total += float(num)
-            num = input("Enter a number or press Enter to quit: ")
-
-        print("The sum is", total, "\nThe average is", total / i)
-
-
-new()
+    print("The sum is", total, "\nThe average is", total / i)
